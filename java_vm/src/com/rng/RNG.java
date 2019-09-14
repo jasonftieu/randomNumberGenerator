b50547e1f16fac47c70f646476c7135bc0b38cb2
Mason <mwitcraft11@gmail.com>
2019-09-14 14:46:39 -0500

    Added files for Java VM App

---
+++ java_vm/src/com/rng/RNG.java
@@ -0,0 +1,31 @@
+package com.rng;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RNG
+ */
+@WebServlet("/")
+public class RNG extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		int num = (int)(Math.random() * 1000000);
+		
+		out.println(num);
+	}
+
+}
