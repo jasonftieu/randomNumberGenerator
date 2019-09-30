770e8e516d0b555ff67c7bc77819da0b145b8016
brettnorton7 <43380203+brettnorton7@users.noreply.github.com>
2019-09-30 14:26:36 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import random
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return str(random.randrange(0,1000000))
-    
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
